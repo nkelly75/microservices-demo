64e1f8ee4234dddc69411732e659c42cb907f739
Niall Kelly <nkelly75@gmail.com>
2020-04-23 14:22:04 +0100

    Add fibservice with OTel straight to Lightstep and include lightstep in collector config

---
+++ src/fibservice/README.md
@@ -0,0 +1,11 @@
+# Fibonacci OTel demo
+
+Node.js OTel example from observe2020.io without babel.
+
+```
+export LIGHTSTEP_KEY=...
+npm run start
+```
+
+http://127.0.0.1:3000/fib?i=10&parallel=false
+http://127.0.0.1:3000/fib?i=10&parallel=true
---
+++ src/fibservice/index.js
@@ -0,0 +1,84 @@
+const tracer = require('./tracing')('adaptive-jazzy-silicon-k')
+const express = require('express');
+const url = require('url');
+const http = require('http');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/foo', (req, res) => {
+  res.send("hello!")
+})
+
+app.get(['/fib'], async (req, res) => {
+  console.log(`/fib ${req.query.i}`);
+  if (req.query.parallel === 'true') {
+    tracer.getCurrentSpan().setAttribute("isParallel", true)
+    req.url = '/fibParallel'
+  } else {
+    tracer.getCurrentSpan().setAttribute("isParallel", false)
+    req.url = '/fibInternal'
+  }
+  if (req.query.i) {
+    tracer.getCurrentSpan().setAttribute("iOuter", req.query.i)
+  }
+  return app._router.handle(req, res)
+})
+
+
+app.get(['/fibInternal'], async (req, res) => {
+  // console.log(`/fibInternal ${req.query.i}`)
+  let initialValue = parseInt(req.query.i);
+  let returnValue = 0
+  if ((initialValue === 0) || (initialValue === 1)) {
+    returnValue = 0
+  } else if (initialValue === 2) {
+    returnValue = 1
+  } else {
+    let minusOneReturn = await makeRequest(`http://127.0.0.1:3090/fibInternal?i=${initialValue - 1}`)
+    let minusTwoReturn = await makeRequest(`http://127.0.0.1:3090/fibInternal?i=${initialValue - 2}`)
+    returnValue = minusOneReturn + minusTwoReturn 
+  }
+
+  res.send(returnValue.toString())
+});
+
+app.get('/fibParallel', async (req, res) => {
+  let initialValue = parseInt(req.query.i)
+  let returnValue = 0
+  if ((initialValue === 0) || (initialValue === 1)) {
+    returnValue = 0
+  } else if (initialValue === 2) {
+    returnValue = 1
+  } else {
+    let [resultMinusOne, resultMinusTwo] = await Promise.all([
+      makeRequest(`http://127.0.0.1:3090/fibParallel?i=${initialValue - 1}`),
+      makeRequest(`http://127.0.0.1:3090/fibParallel?i=${initialValue - 2}`)
+    ])
+    returnValue = resultMinusOne + resultMinusTwo 
+  }
+
+  res.send(returnValue.toString())
+})
+
+function makeRequest(url) {
+  return new Promise ((resolve, reject) => {
+    let data = '';
+    http.get(url, (res) => {
+      res.on('data', (chunk) => {
+        data += chunk
+      })
+      res.on('end', () => {
+        resolve(parseInt(data))
+      })
+      res.on('error', err => {
+        reject(err)
+      })
+    });
+  });
+}
+
+app.listen(process.env.PORT || 3090, () => console.log("Listening to port 3090"));
